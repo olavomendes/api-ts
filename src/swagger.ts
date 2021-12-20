d74ddf977ebf2ff20d5bee7b6243896f28a9267e
Olavo <olavo.77@hotmail.com>
2021-12-20 17:12:24 -0300

    feat: adicionada documentação das tags

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,25 +1,36 @@
-import UserDefinition from "./docs/user/definition";
-import CreateUser from "./docs/user/createUser";
+import UserDefinition from './docs/user/definition';
+import CreateUser from './docs/user/createUser';
+import ListUsers from './docs/user/listUsers';
+
+import TagDefinition from './docs/tag/definition';
+import CreateTag from './docs/tag/createTag';
+import ListTags from './docs/tag/listTags';
 
 const swagger = {
-    swagger: "2.0",
+    swagger: '2.0',
     info: {
-        version: "1.0.0",
-        title: "Documentação da API",
-        description: "Descrição de todas as rotas da API",
+        version: '1.0.0',
+        title: 'Documentação da API',
+        description: 'Descrição de todas as rotas da API',
         contact: {
-            email: "olavo.77@hotmail.com",
+            email: 'olavo.77@hotmail.com',
         },
     },
-    schemes: ["https", "http"],
-    basePath: "/api",
+    schemes: ['https', 'http'],
+    basePath: '/api',
     paths: {
-        "/users": {
+        '/users': {
             post: CreateUser.createUser,
+            get: ListUsers.listUsers,
+        },
+        '/tags': {
+            post: CreateTag.createTag,
+            get: ListTags.listTags,
         },
     },
     definitions: {
         User: UserDefinition.User,
+        Tag: TagDefinition.Tag,
     },
 };
 
