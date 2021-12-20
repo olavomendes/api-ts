069bbb910764e9296c279f545f428c932794b90f
Olavo <olavo.77@hotmail.com>
2021-12-20 17:01:20 -0300

    feat: adicionada documentação da criação de usuário

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,21 +1,26 @@
-import userDefinition from "./docs/user/definition";
+import UserDefinition from "./docs/user/definition";
+import CreateUser from "./docs/user/createUser";
 
 const swagger = {
-  swagger: "2.0",
-  info: {
-    version: "1.0.0",
-    title: "Documentação da API",
-    description: "Descrição de todas as rotas da API",
-    contact: {
-      email: "olavo.77@hotmail.com",
+    swagger: "2.0",
+    info: {
+        version: "1.0.0",
+        title: "Documentação da API",
+        description: "Descrição de todas as rotas da API",
+        contact: {
+            email: "olavo.77@hotmail.com",
+        },
     },
-  },
-  schemes: ["https", "http"],
-  basePath: "/api",
-  paths: {},
-  definitions: {
-    User: userDefinition.User,
-  },
+    schemes: ["https", "http"],
+    basePath: "/api",
+    paths: {
+        "/users": {
+            post: CreateUser.createUser,
+        },
+    },
+    definitions: {
+        User: UserDefinition.User,
+    },
 };
 
 export default swagger;
