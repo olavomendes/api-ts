645d8bf76114b7a2b8c7996cd81a69f35b25248b
Olavo <olavo.77@hotmail.com>
2021-12-25 20:32:13 -0300

    feat: adicionado log quando o servidor é iniciado

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
-import express, { Request, Response, NextFunction } from "express";
-import "express-async-errors";
-import "reflect-metadata";
-import { router } from "./routes";
-import swaggerjson from "./swagger";
-import SwaggerUI = require("swagger-ui-express");
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import 'reflect-metadata';
+import { router } from './routes';
+import swaggerjson from './swagger';
+import SwaggerUI = require('swagger-ui-express');
 
-import "./database";
+import './database';
+import logger from './logger';
 
 const app = express();
 
@@ -13,17 +14,18 @@ app.use(express.json());
 app.use(router);
 
 app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof Error) {
-      return response.status(400).json({ error: err.message });
-    }
-    return response.status(500).json({
-      status: "error",
-      message: "Erro no servidor interno",
-    });
-  }
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+        if (err instanceof Error) {
+            return response.status(400).json({ error: err.message });
+        }
+        return response.status(500).json({
+            status: 'error',
+            message: 'Erro no servidor interno',
+        });
+    },
 );
-app.use("/api/docs", SwaggerUI.serve, SwaggerUI.setup(swaggerjson));
+app.use('/api/docs', SwaggerUI.serve, SwaggerUI.setup(swaggerjson));
 
 const port = process.env.APP_PORT;
 app.listen(port, () => console.log(`Servidor iniciado em "localhost:${port}"`));
+logger.info(`Servidor iniciado em "localhost:${port}"`);
