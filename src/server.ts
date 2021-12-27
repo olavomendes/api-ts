4eeb1be1239ef035ac47090d6b4b209586bdc9f3
Olavo <olavo.77@hotmail.com>
2021-12-27 17:07:44 -0300

    fix: adicionado '/api' nas rotas

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ import logger from './logger';
 const app = express();
 
 app.use(express.json());
-app.use(router);
+app.use('/api', router);
 
 app.use(
     (err: Error, request: Request, response: Response, next: NextFunction) => {
